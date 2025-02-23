beb7f804ff1bdc3f34707a7604d9b135540b1766
UdayDocs <137787964+UdayDocs@users.noreply.github.com>
2025-02-23 19:45:27 +0530

    Added css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import FunctionalInput from './components/FunctionalInput';
 import ClassInput from './components/ClassInput';
-import './style.css';
+import './App.css';
 
 export default function App() {
   return (
-    <>
+    < div className="container">
       <FunctionalInput name="Functional component!" />
       <div className="divider" />
       <ClassInput name="Class based component!" />
-    </>
+    </div>
   );
 }
